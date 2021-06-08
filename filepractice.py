ae54f2b372ddfc5fcd53dd480a6f792c040e53b4
pawan290 <85567025+pawan290@users.noreply.github.com>
2021-06-08 19:53:18 +0530

    my 2nd file

---
+++ filepractice.py
@@ -0,0 +1,67 @@
+# x= "tin"
+# x= "flowers"
+# print(x)
+
+# x= str(5)
+# y=int(8)
+# z=float(3.2354471)
+# print(x)
+# print(y)
+# print(z)
+
+# print(str(5))
+
+
+# x= "pawan"
+# y= 8
+# z= 3.14
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x=["a" , "b" , "c"]
+# s , d , f = x
+# print(x)
+
+# x=["a" , "b" , "c"]
+# x.insert(0,"gig")
+# print(x)
+
+# x=["a" , "b" , "c"]
+# x.append("dig")
+# print(x)
+
+# x= True
+# y= False
+# print(type(x))
+# print(type(y))
+
+# x=["a" , "b" , "c"]
+# print(x[0])
+# print(x[1])
+# print(x[2])
+
+
+# x=["a" , "b" , "c"]
+# print(x[1:3])
+# print(x[:3])
+# print(x[2:])
+# print(x[0:2])
+
+
+# x= "pawan"
+# print(x.upper())
+
+# x= "KUMAR"
+# print(x.lower())
+
+# x= "a , b , c"
+# print(x.replace("a" , "2"))
+
+x = ["cat", "lion" , "cam" , "kite"]
+z = [] 
+for y in x: 
+    y+= " animal"
+    z.append(y)
+    print(z) 
+
