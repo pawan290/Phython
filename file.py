817303a28a9ab99114147a7dae88fba0bd3280eb
pawan290 <pawandiscoveryhbo@gmail.com>
2021-06-08 19:47:42 +0530

    my first commit

---
+++ file.py
@@ -0,0 +1,36 @@
+# x = ["cat", "lion" , "cam" , "kite"]
+# z = [] 
+# for y in x: 
+#     y+= " animal"
+#     z.append(y)
+#     print(z) 
+    
+
+# x = ["cat", "lion" , "cam" , "kite"]
+# for y in x:
+#     y+= " animal"
+#     print(y)
+
+
+# x = [1 , 2 , 3 , 4]
+# z = []
+# for y in x:
+#     y*= 5
+#     z.append(y)
+# print(z)
+
+
+
+
+# a = 12
+# b = 6
+# if a < b:
+#     print("a")
+# elif a > b:
+#     print("b")
+# else:
+#     print("c")
+
+
+
+
